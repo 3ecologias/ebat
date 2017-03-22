826745aef4a05b5d2326c152627509d35a7f79cc
3E <root@3ecologias.net>
2017-03-22 13:44:59 +0000

    core server

---
+++ core/forms.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from django import forms
+from django.core.mail import EmailMessage, BadHeaderError
+
+class Call_me(forms.Form):
+    phone = forms.CharField(required=True,label='', widget=forms.TextInput(
+    attrs={'class':'form-control','placeholder':'(XX) X XXXX XXXX', 'aria-describedby':'basic-addon2'}))
+
+    def send_email(self):
+        phone = self.cleaned_data['phone']
+        subject = 'ME LIGUE!'
+
+        try:
+            email = EmailMessage(subject+" ,Telefone "+phone, phone, 'ebat.atendimento@gmail.com',['ebat.atendimento@gmail.com'])
+            email.content_subtype = "html"
+            email.send()
+
+        except BadHeaderError:
+            raise ValidationError("Cabeçalho inválido")
