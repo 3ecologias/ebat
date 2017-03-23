16e98e2bc09a0ed4d47ccab2cc2e5bb3ed3f6989
lucazartu <lucazartu@gmail.com>
2017-03-23 14:30:11 -0300

    Message configurada

--- core/views.py
+++ core/views.py
@@ -9,10 +9,10 @@ from .forms import Call_me
 class Index(generic.FormView):
 	template_name = 'core/index.html'
 	form_class = Call_me
-	success_url = '/'
+	success_url = '/#call-us'
 	def form_valid(self, form):
 		form.send_email()
-		messages.success(self.request, 'Tudo certo! Em instantes iremos ligar pra você, não se preocupe!')
+		messages.success(self.request, 'Tudo certo! Em instantes ligaremos para você, não se preocupe!')
 		return super(Index, self).form_valid(form)
 
 	def form_invalid(self, form):
