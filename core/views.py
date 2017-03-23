7106a9f1f53c83334a33b3b58291207d4e6f2a78
lucazartu <lucazartu@gmail.com>
2017-03-23 14:20:27 -0300

    Message funcionando

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+# _*_ coding: utf-8 _*_
 from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponse
 from django.template.loader import get_template
 from django.contrib import messages
@@ -11,4 +12,9 @@ class Index(generic.FormView):
 	success_url = '/'
 	def form_valid(self, form):
 		form.send_email()
+		messages.success(self.request, 'Tudo certo! Em instantes iremos ligar pra você, não se preocupe!')
 		return super(Index, self).form_valid(form)
+
+	def form_invalid(self, form):
+		messages.error(self.request, 'Algo de errado aconteceu, tente utilizar o nosso serviço de chat.')
+		return super(Inxed, self).form_invalid(form)
